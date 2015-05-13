af9d2370d634463a457ea67097bb46d8cdb2032b
mconiaris <mconiaris@gmail.com>
2015-05-12 21:16:46 -0400

    Created method to generate wrestler objects and put them in an array.

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,46 @@
+require_relative 'wrestler'
 require 'pry'
 require 'csv'
 
-wrestlers_csv_array = CSV.read("resources/wrestler_list.csv")
-binding.pry
+def turn_array_into_objects(array)
+
+  # Get array from CSV
+  wrestlers_csv_array = array
+
+  # Outgoing Wrestler Objects
+  wrestler_objects_array = Array.new
+
+  wrestlers_csv_array.each do |cell|
+
+    # Create Wrestler objects
+    @wrestler = Wrestler.new(
+
+      first:          cell[0],
+      last:           cell[1],
+      priority:       cell[2],
+      tag_priority:   cell[3],
+      height:         cell[4],
+      weight:         cell[5],
+      weight_class:   cell[6],
+      city:           cell[7],
+      state:          cell[8],
+      country:        cell[9],
+      region:         cell[10],
+      wins:           cell[11],
+      losses:         cell[12],
+      draws:          cell[13],
+      difference:     cell[14],
+      matches:        cell[15],
+      remaining:      cell[16],
+      status:         cell[17],
+      aliases:        cell[18],
+    )
+    wrestler_objects_array.push(@wrestler)
+  end
+  binding.pry
 puts "Testing"
+end
+
+
+@wrestlers_csv_array = CSV.read("resources/wrestler_list.csv")
+turn_array_into_objects(@wrestlers_csv_array)
