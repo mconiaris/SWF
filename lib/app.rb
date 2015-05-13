34d2eb628619c1fad6a6aa782e3b4b07876c3b68
mconiaris <mconiaris@gmail.com>
2015-05-12 21:41:08 -0400

    Created weight_class method to automatically generate the value

--- lib/app.rb
+++ lib/app.rb
@@ -2,13 +2,29 @@ require_relative 'wrestler'
 require 'pry'
 require 'csv'
 
+def weight_class(cell)
+  if cell[5].to_i < 215
+    "Lightweight"
+  elsif cell[5].to_i < 230
+    "Cruiserweight"
+  elsif cell[5].to_i < 300
+    "Heavyweight"
+  elsif cell[5].to_i > 300
+    "Super Heavyweight"
+  else
+    "Heavyweight"
+
+  end
+  # binding.pry
+end
+
 def turn_array_into_objects(array)
 
   # Get array from CSV
   wrestlers_csv_array = array
 
   # Outgoing Wrestler Objects
-  wrestler_objects_array = Array.new
+  @wrestler_objects_array = Array.new
 
   wrestlers_csv_array.each do |cell|
 
@@ -21,7 +37,7 @@ def turn_array_into_objects(array)
       tag_priority:   cell[3],
       height:         cell[4],
       weight:         cell[5],
-      weight_class:   cell[6],
+      weight_class:   weight_class(cell),
       city:           cell[7],
       state:          cell[8],
       country:        cell[9],
@@ -35,12 +51,12 @@ def turn_array_into_objects(array)
       status:         cell[17],
       aliases:        cell[18],
     )
-    wrestler_objects_array.push(@wrestler)
+    @wrestler_objects_array.push(@wrestler)
   end
 end
 
 
 @wrestlers_csv_array = CSV.read("resources/wrestler_list.csv")
-@wrestlers = turn_array_into_objects(@wrestlers_csv_array)
+turn_array_into_objects(@wrestlers_csv_array)
 binding.pry
 puts "Testing"
