f5ea77088614c64d1112da4aace9fc6a049edd74
mconiaris <mconiaris@gmail.com>
2015-05-14 10:22:40 -0400

    Created tests for Wrester's priority

--- spec/wrester_spec.rb
+++ spec/wrester_spec.rb
@@ -4,10 +4,11 @@ require 'pry'
 RSpec.describe 'Wrestler', :wrestler do
   before(:context) do
     @wrestler = Wrestler.new({
-      first:  'John',
-      last:   'Cena',
+      first:      'John',
+      last:       'Cena',
+      priority:   '5',
     })
-    # :last, :priority, :tag_priority,
+    # :priority, :tag_priority,
     # :height, :weight, :class, :city, :state, :country,
     # :region, :wins, :losses, :draws, :difference,
     # :matches, :remaining, :status, :aliases
@@ -19,6 +20,12 @@ RSpec.describe 'Wrestler', :wrestler do
     end
     it "last returns 'Cena'" do
       expect(@wrestler.last).to eq('Cena')
+    end
+    it "priority returns '5'" do
+      expect(@wrestler.priority).to eq(5)
+    end
+    it "priority returns a Fixnum" do
+      expect(@wrestler.priority.class).to eq(Fixnum)
     end
   end
 end
