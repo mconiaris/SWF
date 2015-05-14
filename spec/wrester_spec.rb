be24f4df3129076bd60869493e7268207162c8de
mconiaris <mconiaris@gmail.com>
2015-05-14 10:34:54 -0400

    Created test to check weight

--- spec/wrester_spec.rb
+++ spec/wrester_spec.rb
@@ -8,10 +8,11 @@ RSpec.describe 'Wrestler', :wrestler do
       last:           'Cena',
       priority:       '5',
       tag_priority:   '2',
-      height:         "6' 1\""
+      height:         "6' 1\"",
+      weight:         "251",
+      #, :class, :city, :state, :country,
 
     })
-    # :height, :weight, :class, :city, :state, :country,
     # :region, :wins, :losses, :draws, :difference,
     # :matches, :remaining, :status, :aliases
 
@@ -38,6 +39,9 @@ RSpec.describe 'Wrestler', :wrestler do
     # TODO: Fix test to respond to correct input
     it "height returns 6' 1\"" do
       expect(@wrestler.height).to eq("6' 1")
+    end
+    it "weight returns 251" do
+      expect(@wrestler.weight).to eq(251)
     end
   end
 end
