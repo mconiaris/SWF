ffb5363530b012070d028aefb78e3464fca6f796
mconiaris <mconiaris@gmail.com>
2015-05-14 10:19:24 -0400

    Creates test for Wrestler last name

--- spec/wrester_spec.rb
+++ spec/wrester_spec.rb
@@ -2,6 +2,23 @@ require 'wrestler'
 require 'pry'
 
 RSpec.describe 'Wrestler', :wrestler do
+  before(:context) do
+    @wrestler = Wrestler.new({
+      first:  'John',
+      last:   'Cena',
+    })
+    # :last, :priority, :tag_priority,
+    # :height, :weight, :class, :city, :state, :country,
+    # :region, :wins, :losses, :draws, :difference,
+    # :matches, :remaining, :status, :aliases
+
+  end
   describe '#initialize' do
+    it "first returns 'John'" do
+      expect(@wrestler.first).to eq('John')
+    end
+    it "last returns 'Cena'" do
+      expect(@wrestler.last).to eq('Cena')
+    end
   end
 end
