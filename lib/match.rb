88b25cbccc0841d44b03f74e84f6db7f38c7c760
mconiaris <mconiaris@gmail.com>
2015-05-16 16:35:42 -0400

    Added match_type variable to Match class

--- lib/match.rb
+++ lib/match.rb
@@ -1,9 +1,10 @@
 class Match
 
-  attr_reader :wrestler_1, :wrestler_2, :winner,
-    :outcome, :time
+  attr_reader :match_type, :wrestler_1, :wrestler_2,
+    :winner, :outcome, :time
 
   def initialize(args)
+    @match_type         =  args[:match_type]
     @wrestler_1         =  args[:wrestler_1]
     @wrestler_2         =  args[:wrestler_2]
     @winner             =  args[:winner]
