8d40bb845cb1f88a0248da9e79512a42696c5d16
mconiaris <mconiaris@gmail.com>
2015-05-16 16:26:39 -0400

    Fixed error that was bombing match initalization.

--- lib/match.rb
+++ lib/match.rb
@@ -4,10 +4,10 @@ class Match
     :outcome, :time
 
   def initialize(args)
-    @wrestler_1         =  args[:wrestler_1],
-    @wrestler_2         =  args[:wrestler_2],
-    @winner             =  args[:winner],
-    @outcome            =  args[:outcome],
-    @time               =  args[:time],
+    @wrestler_1         =  args[:wrestler_1]
+    @wrestler_2         =  args[:wrestler_2]
+    @winner             =  args[:winner]
+    @outcome            =  args[:outcome]
+    @time               =  args[:time]
   end
 end
