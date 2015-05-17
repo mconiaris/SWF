72c12db51518e062f3d046495ccd15a99cd19324
mconiaris <mconiaris@gmail.com>
2015-05-17 08:29:20 -0400

    Created first test for arena

---
+++ spec/arena_spec.rb
@@ -0,0 +1,19 @@
+require 'pry'
+require 'date'
+
+
+RSpec.describe 'Arena', :arena do
+  before(:context) do
+    @arena = Arena.new({
+      arena_name:     'Madison Square Garden',
+      arena_city:     'New York',
+      arena_sate:     'NY',
+      arena_country:  'United States',
+    })
+  end
+  describe '#initialize' do
+    it "returns a Arena object" do
+      expect(@arena.class).to eq(Arena)
+    end
+  end
+end
