8412f43db17723f752888b7ea9c57e6c1218d126
mconiaris <mconiaris@gmail.com>
2015-05-17 08:02:09 -0400

    Created tests to check for date

--- spec/match_spec.rb
+++ spec/match_spec.rb
@@ -34,6 +34,12 @@ RSpec.describe 'Match', :match do
     it "time returns '12:52'" do
       expect(@match.time).to eq('12:52')
     end
+    it "returns a date object" do
+      expect(@match.date.class).to eq(Date)
+    end
+    it "date equals 2015-05-18" do
+      expect(@match.date).to eq('2015-05-18')
+    end
   end
   #TODO: create tests for date, location, card name
 end
